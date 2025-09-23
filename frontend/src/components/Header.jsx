f36d29072cf0e2048eb967a5dc704a459b154245
Karthikkumar75 <wicky7569@gmail.com>
2025-09-23 12:22:24 +0530

    logo changes and katex

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <AppBar position="sticky" elevation={0}>
       <Toolbar>
         <img
-          src="/sllogo.png"
+          src="/logo192.png"
           alt="Logo"
           style={{ width: '60px', height: '60px', marginRight: '16px', backgroundColor: 'white', padding: '5px' }}
         />
