4224c26e489d8562e4f11d6587051d6bb158f4c4
Karthikkumar75 <wicky7569@gmail.com>
2025-09-25 03:10:40 +0530

    logo updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -32,7 +32,7 @@ export default function Footer() {
             <Grid item xs={12} sm={4}>
               <Box display="flex" alignItems="center" mb={2}>
                 <img
-                  src="/logo192.png"
+                  src="/SLlogo.png"
                   alt="Logo"
                   style={{ width: '50px', height: '50px', marginRight: '10px', backgroundColor: 'white', padding: '4px', borderRadius: '8px' }}
                 />
