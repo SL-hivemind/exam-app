cd4b6e9b56e39c06d69198896e4e2fa9758fa98e
Karthikkumar75 <wicky7569@gmail.com>
2025-10-26 21:05:37 +0530

    gevent is added

--- backend/app.py
+++ backend/app.py
@@ -45,8 +45,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "super-secret")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", secrets.token_hex(16))
 
-app.config['SQLALCHEMY_POOL_SIZE'] = 5
-app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30
+app.config['SQLALCHEMY_POOL_SIZE'] = 200
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 10
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 300
 
 # ----- Logging -----
