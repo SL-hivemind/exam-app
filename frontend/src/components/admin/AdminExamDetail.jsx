73a7f15f995d7c97c0c2ec3796f7f9ed4322ef8d
Karthikkumar75 <wicky7569@gmail.com>
2026-03-28 16:52:34 +0530

    classes updated at assign level

--- frontend/src/components/admin/AdminExamDetail.jsx
+++ frontend/src/components/admin/AdminExamDetail.jsx
@@ -529,6 +529,11 @@ export default function AdminExamDetail() {
                         onChange={(e) => setAssignClass(e.target.value)}
                     >
                         <MenuItem value=""><em>All Classes</em></MenuItem>
+                        <MenuItem value="1">1</MenuItem>
+                        <MenuItem value="2">2</MenuItem>
+                        <MenuItem value="3">3</MenuItem>
+                        <MenuItem value="4">4</MenuItem>
+                        <MenuItem value="5">5</MenuItem>
                         <MenuItem value="6">6</MenuItem>
                         <MenuItem value="7">7</MenuItem>
                         <MenuItem value="8">8</MenuItem>
