91a0e0c40b88ea1df992410388c89978d0ebf999
Karthikkumar75 <wicky7569@gmail.com>
2026-03-28 16:38:10 +0530

    class in student register form are added

--- frontend/src/components/admin/AdminStudents.jsx
+++ frontend/src/components/admin/AdminStudents.jsx
@@ -471,6 +471,11 @@ export default function AdminStudents() {
                     label="Class"
                     onChange={(e) => setCurrent({ ...current, class_number: e.target.value })}
                   >
+                    <MenuItem value="1">1</MenuItem>
+                    <MenuItem value="2">2</MenuItem>
+                    <MenuItem value="3">3</MenuItem>
+                    <MenuItem value="4">4</MenuItem>
+                    <MenuItem value="5">5</MenuItem>
                     <MenuItem value="6">6</MenuItem>
                     <MenuItem value="7">7</MenuItem>
                     <MenuItem value="8">8</MenuItem>
